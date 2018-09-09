2263af3419f46ba97857d65bb32944960b38004b
yexiaosong <mryexiaosong@live.com>
2018-09-09 20:30:56 +0800

    基本的历史记录功能完成

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,65 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { commands, window } from 'vscode';
-
+import { commands, window, Range, Position, workspace, TextEditor, ExtensionContext } from 'vscode';
+let config = workspace.getConfiguration('clipboard');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-clipboard-with-formatter" is now active!');
+    let clipboardHistory: Array<any> = [];
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
+    function addToClipboard(editor: TextEditor) {
+        const selections = editor.selections;
+        const document = editor.document;
+        let lineStart: Position, lineEnd: Position;
+        let selectedContent: string;
+        selections.forEach(selection => {
+            const line = selection.active.line;
+
+            lineStart = !selection.isEmpty ? selection.start
+                    : new Position(line, 0);
+            lineEnd = !selection.isEmpty ? selection.end
+                    : new Position(line, document.lineAt(line).range.end.character);
+
+            selectedContent = document.getText(new Range(lineStart, lineEnd));
+            if(clipboardHistory.indexOf(selectedContent) === -1) {
+                clipboardHistory.push(selectedContent);
+            }
+        });
+    }
+
+    function pasteSelected(context) {
+        let editor = window.activeTextEditor;
+        if(editor) {
+            editor.edit(doc => {
+                doc.insert(editor.selection.start, context);
+            });
+        }
+    }
+
     const copy = commands.registerCommand('extension.copy', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        window.showInformationMessage('extension.copy');
+        addToClipboard(window.activeTextEditor);
+        commands.executeCommand("editor.action.clipboardCopyAction");
     });
 
     const cut = commands.registerCommand('extension.cut', () => {
         window.showInformationMessage('extension.cut');
+        commands.executeCommand("editor.action.clipboardCutAction");
     });
 
     const paste = commands.registerCommand('extension.paste', () => {
-        window.showInformationMessage('extension.paste');
+        commands.executeCommand("editor.action.clipboardPasteAction");
     });
 
+    const showClipboardHistory = commands.registerCommand('extension.clipboardHistory', () => {
+        if(!clipboardHistory.length){
+            window.showInformationMessage('当前剪贴板内容为空');
+        }
+        window.showQuickPick(clipboardHistory).then(item => {
+            pasteSelected(item);
+        });
+    });
 
-    context.subscriptions.concat([copy, cut, paste]);
+    context.subscriptions.concat([copy, cut, paste, showClipboardHistory]);
 }
 
 // this method is called when your extension is deactivated
